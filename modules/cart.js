eac4feba52f67c18e8626dc2476a5e7302500960
Ruben-Glorian <ruben.glorian9@etu.univ-lorraine.fr>
2025-02-11 16:46:56 +0100

    Exercise 6: Created the object Cart.

---
+++ modules/cart.js
@@ -0,0 +1,16 @@
+class Cart {
+    constructor() {
+        this.contenu = [];
+    }
+
+    addToCart(product) {
+        const existingProduct = this.contenu.find(item => item.product.ref === product.ref);
+        if (existingProduct) {
+            existingProduct.qty += 1;
+        } else {
+            this.contenu.push({ product, qty: 1 });
+        }
+    }
+}
+
+export default Cart ;
