9fead0f1a27dc4cd570addbb2e24b951290405d0
Ruben-Glorian <ruben.glorian9@etu.univ-lorraine.fr>
2025-02-11 14:34:19 +0100

    Exercise 2: built a visual of a product list.

--- modules/ui.js
+++ modules/ui.js
@@ -1,27 +1,27 @@
-import Products from "./products";
+import Products from "./products.js";
 
 function displayProduct(product) {
     const list = document.getElementById('product-list');
     list.innerHTML += `<div className="product">
-        <div className="photo">
-            picto
-            <a className="product-add2cart">
-                <span className="mdi mdi-cart"></span>
-            </a>
-        </div>
-        <div className="details">
-            <div className="details-top">
-                <strong className="bigger">${product.ref}</strong>
-                <strong className="bigger">${product.price} €</strong>
+            <div className="photo">
+                <img src="${product.image}" alt="${product.ref}">
+                <a className="product-add2cart">
+                    <span className="mdi mdi-cart"></span>
+                </a>
             </div>
-            <div className="details-description">
-                <p>${product.description}</p>
+            <div className="details">
+                <div className="details-top">
+                    <strong className="bigger">${product.ref}</strong>
+                    <strong className="bigger">${product.price} €</strong>
+                </div>
+                <div className="details-description">
+                    <p>${product.description}</p>
+                </div>
             </div>
         </div>` ;
 }
 
 function buildProductsList(Products) {
-
     for (let product of Products) {
         displayProduct(product) ;
     }
